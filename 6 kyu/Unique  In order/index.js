8309f581e890f47abe26ea2013ca37af237263dc
Maksim Laurouski <maximlavrovsky@mail.ru>
2024-03-04 19:15:12 +0300

    feat: Unique in order

---
+++ 6 kyu/Unique  In order/index.js
@@ -0,0 +1,16 @@
+var uniqueInOrder = function (iterable) {
+    return Array.isArray(iterable)
+        ? iterable.filter((el, index) => el !== iterable[index + 1])
+        : iterable.split('').filter((el, index) => el !== iterable[index + 1])
+}
+
+console.log(uniqueInOrder('ABBCcAD'))
+
+// Implement the function unique_in_order which takes as argument a sequence and returns a list of items without
+// any elements with the same value next to each other and preserving the original order of elements.
+//
+//     For example:
+//
+//     uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+// uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+// uniqueInOrder([1,2,2,3,3])       == [1,2,3]
